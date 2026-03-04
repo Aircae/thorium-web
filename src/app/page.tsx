80276d2b5c59d88d9919e1c3ecfb34cdabf095a9
媣. <f2180189549@163.com>
2026-03-05 05:17:24 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,60 +11,20 @@ import "./home.css";
 
 const books = [
   {
-    title: "Moby Dick",
-    author: "Herman Melville",
-    cover: "/images/MobyDick.jpg",
-    url: "/read/moby-dick",
-    rendition: "Reflowable EPUB"
-  },
-  {
-    title: "The House of the Seven Gables",
-    author: "Nathaniel Hawthorne",
-    cover: "/images/TheHouseOfTheSevenGables.jpg",
-    url: "/read/the-house-of-seven-gables",
-    rendition: "Reflowable EPUB"
-  },
-  {
-    title: "Les Diaboliques",
-    author: "Jules Barbey d'Aurevilly",
-    cover: "/images/LesDiaboliques.png",
-    url: "/read/les-diaboliques",
-    rendition: "Reflowable EPUB"
-  },
-  {
-    title: "Bella the Dragon",
-    author: "Barbara Nick, Elaine Steckler",
-    cover: "/images/Bella.jpg",
-    url: "/read/bella-the-dragon",
-    rendition: "Fixed-Layout EPUB"
+    title: "掉入异世界也要努力活下去",
+    author: "梦丣",
+    cover: "/images/掉入异世界也要努力活下去.jpg",
+    url: "/read/掉入异世界也要努力活下去",
+    rendition: "EPUB"
   }
 ];
 
 const onlineBooks = [
-  {
-    title: "Accessible EPUB3",
-    author: "Matt Garrish",
-    cover: "/images/accessibleEpub3.jpg",
-    url: "/read/manifest/https%3A%2F%2Fpublication-server.readium.org%2Fwebpub%2FaHR0cHM6Ly9naXRodWIuY29tL0lEUEYvZXB1YjMtc2FtcGxlcy9yZWxlYXNlcy9kb3dubG9hZC8yMDIzMDcwNC9hY2Nlc3NpYmxlX2VwdWJfMy5lcHVi%2Fmanifest.json",
-    rendition: "Reflowable EPUB"
-  },
-  {
-    title: "Children Literature",
-    author: "Charles Madison Curry, Erle Elsworth Clippinger",
-    cover: "/images/ChildrensLiterature.png",
-    url: "/read/manifest/https%3A%2F%2Fpublication-server.readium.org%2Fwebpub%2FaHR0cHM6Ly9naXRodWIuY29tL0lEUEYvZXB1YjMtc2FtcGxlcy9yZWxlYXNlcy9kb3dubG9hZC8yMDIzMDcwNC9jaGlsZHJlbnMtbGl0ZXJhdHVyZS5lcHVi%2Fmanifest.json",
-    rendition: "Reflowable EPUB"
-  }
+
 ];
 
 const webPublications = [
-  {
-    title: "Readium CSS Implementers’ Documentation",
-    author: "Jiminy Panoz",
-    cover: "/images/readium-css.jpg",
-    url: "/read/experimental/readium-css",
-    rendition: "Web Publication"
-  }
+
 ];
 
 export default function Home() {
